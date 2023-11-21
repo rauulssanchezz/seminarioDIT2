6fec5df0ab46c9cd73fa9b32ce2e7d2a7079943b
EAG <rauulssanchezz@gmail.com>
2023-11-21 18:48:56 +0100

    sadfgklñukjilñ

---
+++ app/src/main/java/com/example/seminariodit2/Ejercicio3.kt
@@ -0,0 +1,17 @@
+package com.example.seminariodit2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.textfield.TextInputEditText
+
+class Ejercicio3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio3)
+    }
+
+    fun enviar(view: View) {
+
+    }
+}
